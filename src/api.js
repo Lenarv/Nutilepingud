923f6c366783911f3cfe19b960f622610ae36f1e
Lenar Valk <valklenar@gmail.com>
2022-09-12 19:38:27 +0300

    5
    5

--- src/api.js
+++ src/api.js
@@ -1,12 +1,23 @@
-const express = require("express");
-const serverless = require("serverless-http");
+const express = require('express');
+const serverless = require('serverless-http')
 
 const app = express();
-const router = express.Router();
+const router = express.Router()
 
-router.get("/:nimi", (req, res) => {
-  res.send("Tere " + req.params.nimi + req.params.nimi + "!")
-});
+router.get('/',(req,res) => {
+  if(req.query.nimi != undefined) {
+    res.send('Tere, ' + req.query.nimi + "!");
+  } else {
+    res.send('Tere, John Doe!')
+  }
+})
+
+router.get('/test',(req,res) => {
+  res.json({
+      hello: "test!"
+    });
+
+})
 
 app.use(`/.netlify/functions/api`, router);
 
